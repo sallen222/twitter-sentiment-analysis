0854000673e54596fa1dd84160743cd2083f99eb
Steven Allen <222sallen@gmail.com>
2022-09-14 12:02:08 -0400

    add args and kinesis integration

--- stream-listener/twitter_listener.py
+++ stream-listener/twitter_listener.py
@@ -1,12 +1,30 @@
+import imp
 import tweepy
 from dotenv import load_dotenv
 import os
 from kinesis_helper import KinesisStream
+from text_transform import transform
+import argparse
 
+# parse keyword to search from command
+def parse_arg():
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--keyword", type=str, required=True)
+    
+    params = vars(parser.parse_args())
+    
+    return params
+
+if __name__ == "__main__":
+    params = parse_arg()
+    global searchTerm
+    searchTerm = params['keyword']
 # Loading bearer token from .env file
 load_dotenv()
 bearer_token= os.getenv('bearer_token')
-
+print(searchTerm)
 # Name of the AWS Kinesis stream
 streamName = "twitter-stream"
 
@@ -17,10 +35,10 @@ class MyStream(tweepy.StreamingClient):
         print("Connected to Twitter API.")
 
     def on_tweet(self, tweet):
-        print(tweet.text)
-        #outputData = {'': 'data'}
-        #stream = KinesisStream(streamName)
-        #stream.send_stream(data=outputData)
+        tweet = transform(tweet.text)
+        output = {'key': f'{searchTerm}', 'content': f'{tweet}'}
+        stream = KinesisStream(streamName)
+        stream.send_stream(data=output)
         return True
 
     def on_error(self, status):
@@ -32,9 +50,6 @@ class MyStream(tweepy.StreamingClient):
         if status == 401:
             print("Authentication Error. Stopping stream.")
             return False
-
-# Placeholder tracking keyword
-searchTerm = ""
 
 rule = tweepy.StreamRule((f"{searchTerm} lang:en -is:retweet -is:reply"))
 
