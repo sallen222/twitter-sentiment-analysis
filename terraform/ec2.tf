e237758ae70edc59113ab643d41a9767329ee4e2
sallen222 <222sallen@gmail.com>
2022-09-18 22:34:15 -0400

    updated user data

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -38,11 +38,10 @@ resource "aws_instance" "listener-instance" {
     sudo apt-get install python3 python3-pip git -y
     sudo apt-get install python3-pip -y
     sudo apt-get install git -y
-    sudo pip3 install tweepy boto3 dotenv
+    sudo pip3 install tweepy boto3 python-dotenv
     git clone https://github.com/sallen222/twitter-sentiment-analysis /home/ubuntu/twitter-sentiment-analysis
-    cd /home/ubuntu/twitter-sentiment-analysis
-
-    
+    cd /home/ubuntu/twitter-sentiment-analysis/stream-listener
+    chmod +x twitter_listener.py
 EOF
 }
 
