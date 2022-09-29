3f2195f01b9f7484e632bfbac903598d88bab1a0
sallen222 <222sallen@gmail.com>
2022-09-29 18:13:42 -0400

    added comments

--- lambda/lambda_comprehend.py
+++ lambda/lambda_comprehend.py
@@ -7,23 +7,29 @@ ddbResource = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
     
+    # Pull object details from event
     eventKey = event['Records'][0]['s3']['object']['key']
     eventBucket = event['Records'][0]['s3']['bucket']['name']
-
+    
+    # Get object from s3 and pull object body
     object = s3Client.get_object(Bucket=eventBucket, Key=eventKey)
     objectContent = object['Body'].read().decode('utf-8')
     print("TWEET CONTENT = " + objectContent)
-
+    
+    # Take values from body and declare them as variables
     objectContent = json.loads(objectContent)
     tweetKey = objectContent['key']
     tweetContent = objectContent['content']
     tweetTimestamp = objectContent['timestamp']
-
+    
+    # Send the tweet content to comprehend for snetiment analysis
     sentiment = comprehendClient.detect_sentiment(Text=tweetContent,LanguageCode='en')['Sentiment']
     print("SENTIMENT = " + sentiment)
-
+    
+    # Upload output to DyanamoDB
     ddbPutItem(tweetKey, tweetContent, sentiment, tweetTimestamp)
-
+    
+    # Delete s3 object
     s3Client.delete_object(Bucket=eventBucket, Key=eventKey)
 
 def ddbPutItem(key, content, sentiment, timestamp):
